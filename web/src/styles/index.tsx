4f51ca06fb86231f46830f4551cb54d87be8db75
ZenoExile <rogerio.piratelli@gmail.com>
2025-04-02 23:59:56 -0300

    Update

--- web/src/styles/index.tsx
+++
@@ -1,5 +0,0 @@
-import borderListStyles from "./border-list-styles.module.css";
-import formStyles from "./form-styles.module.css";
-import interactiveStyles from "./interactive-styles.module.css";
-
-export { formStyles, borderListStyles, interactiveStyles };
